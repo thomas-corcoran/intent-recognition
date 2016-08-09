d0061aea1623bd9e2e85412174bcbe42fa3dc95f
Thomas Corcoran <thomas@fitroom.me>
2016-08-09 14:13:58 -0700

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='intent_recognition',
     version='0.1dev',
-    packages = ['intent']
+    packages = ['intent'],
     install_requires = ['nltk','sklearn']
     )
