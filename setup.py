93a76289b8c48e8c2209c9d572a7baa9911cdd61
Thomas Corcoran <chasecorcoran+gh@gmail.com>
2016-08-09 15:36:32 -0700

    refactoring for packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.core import setup
 setup(
     name='intent_recognition',
     version='0.1dev',
-    packages = ['intent'],
-    install_requires = ['nltk','sklearn']
+    packages = ['intent_recognition',
+                'intent_recognition.intent',
+                'intent_recognition.models'],
+    install_requires = ['nltk==3.2.1','scikit-learn==0.17.1']
     )
